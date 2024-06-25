9b7ac9f33a1d6e49012d8b2d177a218e706d0f9b
laevatein29 <liyongchen29@gmail.com>
2024-06-25 10:43:11 +0800

    feat: add print

--- plugin/show-author.lua
+++ plugin/show-author.lua
@@ -1,3 +1,7 @@
-if vim.fn.has("nvim-0.7.0") ~= 1 then
-	vim.api.nvim_err_writeln("Hardtime.nvim requires at least nvim-0.7.0.")
+if vim.g.loaded_co_author then
+	return
 end
+
+vim.g.loaded_co_author = true
+
+require("show-author")
