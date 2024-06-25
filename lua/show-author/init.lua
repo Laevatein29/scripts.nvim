6b62bbf6d03c789e64c141d1a570df413c8d0a69
laevatein29 <liyongchen29@gmail.com>
2024-06-25 10:38:20 +0800

    feat: add print

---
+++ lua/show-author/init.lua
@@ -0,0 +1,43 @@
+local function has_eslint_config()
+	-- 检查常见 ESLint 配置文件名
+	for _, config_file in ipairs({ ".eslintrc.js", ".eslintrc.json", ".eslintrc", ".eslintrc.yaml", ".eslintrc.yml" }) do
+		if vim.fn.filereadable(config_file) == 1 then
+			return true
+		end
+	end
+	return false
+end
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+	pattern = {
+		"*.vue",
+		"*.js",
+		"*.ts",
+		"*.jsx",
+		"*.tsx",
+		"*.mjs",
+	},
+	command = function()
+		print("ESLint")
+		if has_eslint_config() then
+			vim.cmd(":EslintFixAll")
+		else
+			print("no eslint config file!")
+		end
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = {
+		"vue",
+		"javascript",
+		"javascriptreact",
+		"javascript.jsx",
+		"typescript",
+		"typescriptreact",
+		"typescript.tsx",
+	},
+	callback = function()
+		vim.b.autoformat = false
+	end,
+})
