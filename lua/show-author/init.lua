acc7c9474b12aff7f11d1a2f022a3cfecdd9ea9d
laevatein29 <liyongchen29@gmail.com>
2024-07-03 10:07:37 +0800

    feat: fix order

--- lua/show-author/init.lua
+++ lua/show-author/init.lua
@@ -1,38 +1,3 @@
--- local co_author = {}
---
--- co_author.generate = function()
---     local co_authors = vim.fn.systemlist('git log --format="%aN <%aE>"')
---     local unique_co_authors = {}
---     for _, author in ipairs(co_authors) do
---         if not unique_co_authors[author] then
---             unique_co_authors[author] = true
---             unique_co_authors[#unique_co_authors + 1] = author
---         end
---     end
---     return unique_co_authors
--- end
---
--- co_author.list = function()
---     local co_authors = co_author.generate()
---     local items = {}
---     for _, author in ipairs(co_authors) do
---         items[#items + 1] = author
---     end
---     if #co_authors > 0 then
---         vim.ui.select(items, { prompt = 'Select Co-Author' }, function(item, _)
---             if item ~= nil then
---                 local string = 'Co-authored-by: ' .. item
---                 local line, column = unpack(vim.api.nvim_win_get_cursor(0))
---
---                 vim.api.nvim_buf_set_lines(0, line, line, true, { string })
---                 vim.api.nvim_win_set_cursor(0, { line, column + #string })
---             end
---         end)
---     end
--- end
---
--- return co_author
-
 local Show_author = {}
 
 Show_author.generate = function()
@@ -60,11 +25,13 @@ Show_author.list = function()
 				local string = "show-authored-by: " .. item
 				local filename = vim.fn.expand("%:t")
 				local date = os.date("%Y-%m-%d")
-				vim.fn.append(vim.fn.line("."), string.format(" */"))
-				vim.fn.append(vim.fn.line("."), string.format(" * @filename - %s", filename))
-				vim.fn.append(vim.fn.line("."), string.format(" * @author - %s", item))
-				vim.fn.append(vim.fn.line("."), string.format(" * @date - %s", date))
-				vim.fn.append(vim.fn.line("."), string.format("/**"))
+
+				local current_line = vim.fn.line(".")
+				vim.fn.append(current_line, string.format("--"))
+				vim.fn.append(current_line + 1, string.format("-- @filename - %s", filename))
+				vim.fn.append(current_line + 2, string.format("-- @author - %s", item))
+				vim.fn.append(current_line + 3, string.format("-- @date - %s", date))
+				vim.fn.append(current_line + 4, string.format("--"))
 			end
 		end)
 	end
