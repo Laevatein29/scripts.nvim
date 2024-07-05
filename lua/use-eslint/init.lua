ed4e86331fcd8d0aa995b69aac2086fa5e3bd630
laevatein29 <liyongchen29@gmail.com>
2024-07-05 16:09:41 +0800

    fix: fix formatter

--- lua/use-eslint/init.lua
+++ lua/use-eslint/init.lua
@@ -2,14 +2,17 @@ local use_eslint = {}
 
 local function FixAll()
   -- callback = function() vim.b.autoformat = false end,
-  if vim.b.autoformat then vim.notify("Fixed By Default", vim.log.levels.INFO) end
-  local eslint_exists = vim.fn.exists ":EslintFixAll"
-  -- vim.notify("Hello from Lua!" .. eslint_exists, vim.log.levels.INFO)
-  if eslint_exists > 0 then
-    vim.notify("Eslint Fixed", vim.log.levels.INFO)
-    vim.api.nvim_command ":EslintFixAll"
+  if vim.b.autoformat then
+    vim.notify("Fixed By Default", vim.log.levels.INFO)
   else
-    vim.notify("Eslint config is not found!", vim.log.levels.INFO)
+    local eslint_exists = vim.fn.exists ":EslintFixAll"
+    -- vim.notify("Hello from Lua!" .. eslint_exists, vim.log.levels.INFO)
+    if eslint_exists > 0 then
+      vim.notify("Eslint Fixed", vim.log.levels.INFO)
+      vim.api.nvim_command ":EslintFixAll"
+    else
+      vim.notify("Eslint config is not found!", vim.log.levels.INFO)
+    end
   end
 end
 
