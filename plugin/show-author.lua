bea10b5a1b2c688c48169c5199e92e81f133051b
laevatein29 <liyongchen29@gmail.com>
2024-07-16 14:43:39 +0800

    feat: add todo script

--- plugin/show-author.lua
+++ plugin/show-author.lua
@@ -1,7 +1,6 @@
-if vim.g.show_author then
-	return
-end
+if vim.g.show_author then return end
 
 vim.g.show_author = true
 
 vim.api.nvim_create_user_command("ShowAuthor", 'lua require("show-author").list()', {})
+vim.api.nvim_create_user_command("ShowTodo", 'lua require("show-author").todo()', {})
